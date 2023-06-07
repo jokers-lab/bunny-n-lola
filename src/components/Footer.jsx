ba2d22bf9fd60ebd91419cb3d93a12c3a5c972e8
harshawije-dev <harsha.bro1997@gmail.com>
2023-06-07 15:04:26 +0530

    footer and css update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <div className="footer-base">
-      <p className="text-bottom">Powered by Candy Factory - CyberLabs</p>
+      <p className="text-bottom">Powered by Candy Factory - Cyber Lab</p>
     </div>
   )
 }

6ddfdf534fd61dcc171e6a8238f599c40a06e5d9
harshawije-dev <harsha.bro1997@gmail.com>
2023-06-12 18:52:46 +0530

    undo the process

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
+import logo from "./assets/img/updated-gif.gif";
+import text from "./assets/img/text.png";
 import Footer from "./components/Footer";
-import video from "./assets/video/bnl_base.mp4"
 
 function App() {
   return (
     <div className="main bg-base">
       <section className="section-base">
-        <video className="w-flex" height={300} width={600} autoPlay={true} loop={true} playsInline>
-          <source src={video} type="video/mp4"/>
-        </video>
+        <img src={logo} alt="" className="w-flex" />
+        <img src={text} alt="" className="w-text" />
+        <p>
+          A World of <b>Elephant Hugs</b> and <b>Bunny Smiles!</b>
+        </p>
       </section>
       <article className="bottom-caption relative">
         <a href="http://www.candyfactorylk.com/">Coming Soon</a>
